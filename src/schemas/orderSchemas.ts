aa0184f28098e5932a2452047825099ea244dc23
gcontiero11 <gustcontiero@gmail.com>
2025-08-08 18:58:02 -0300

    feat: creating zod schemas to auxiliate into the code organization

--- src/schemas/orderSchemas.ts
+++ src/schemas/orderSchemas.ts
@@ -1,10 +1,29 @@
 import { z } from "zod";
+import { discountSchema } from "./discountSchemas.ts";
+
+const itemSchema = z.object({
+  productId: z.string(),
+  quantity: z.number(),
+  unitPrice: z.number(),
+  category: z.string()
+});
+
+export type ItemInfos = z.infer<typeof itemSchema>;
 
 const productSchema = z.object({
   productId: z.string(),
   quantity: z.number(),
 });
-
-export const createOrderSchema = z.array(productSchema);
+export const createOrderSchema = z.object({
+  items: z.array(productSchema)
+});
 
 export type CreateOrderReqModel = z.infer<typeof createOrderSchema>;
+
+const createOrderResModel = z.object({
+  items: z.array(itemSchema),
+  discounts: z.array(discountSchema),
+  total: z.number()
+});
+
+export type CreateOrderResModel = z.infer<typeof createOrderResModel>;

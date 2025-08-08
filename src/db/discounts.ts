0b2973a52eecec4b7244bac4ac7be7d0e8fcab3d
gcontiero11 <gustcontiero@gmail.com>
2025-08-08 15:38:57 -0300

    feat: adding discount multiple types of discount

---
+++ src/db/discounts.ts
@@ -0,0 +1,38 @@
+export const discounts = [
+  {
+    code: "QTY_TIE_10PCT",
+    name: "Desconto por volume - 10%",
+    fixed: 0,
+    rate: 0.1
+  },
+  {
+    code: "QTY_TIE_15PCT",
+    name: "Desconto por volume - 15%",
+    fixed: 0,
+    rate: 0.15
+  },
+  {
+    code: "QTY_TIE_20PCT",
+    name: "Desconto por volume - 20%",
+    fixed: 0,
+    rate: 0.2
+  },
+  {
+    code: "CART_VALUE_FIXED_50",
+    name: "Desconto por valor do carrinho - R$50 fixo",
+    fixed: 50,
+    rate: 0
+  },
+  {
+    code: "CART_VALUE_FIXED_150",
+    name: "Desconto por valor do carrinho - R$150 fixo",
+    fixed: 150,
+    rate: 0
+  },
+  {
+    code: "CAT_ACC_5PCT",
+    name: "Categoria acessórios 5%",
+    fixed: 0,
+    rate: 0.05
+  },
+];

d886835d742fe1ca5b8750501e24fb3489672a94
gcontiero11 <gustcontiero@gmail.com>
2025-08-09 18:09:23 -0300

    fix: changing the  hello world endpoint to the ping pong one, to verify if the server is up

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,8 +2,8 @@ import type { FastifyInstance } from "fastify";
 import orderRoutes from "./orderRoutes.ts";
 
 export default async function (app: FastifyInstance) {
-  app.get("/", (req, res) => {
-    res.send("Hello World");
+  app.get("/ping", (req, res) => {
+    res.send("pong");
   });
 
   app.register(orderRoutes, { prefix: "/v1/orders" })

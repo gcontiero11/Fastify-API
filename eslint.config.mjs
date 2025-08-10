a242a61ee1923b820523c83194abd4baf98631e8
gcontiero11 <gustcontiero@gmail.com>
2025-08-09 21:12:50 -0300

    test: configuring jest into the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-// import jest from "eslint-plugin-jest";
+import jest from "eslint-plugin-jest";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
@@ -13,14 +13,14 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  // {
-  //   files: ["src/tests/**/*.{js,ts}"],
-  //   ...jest.configs["flat/recommended"],
-  //   rules: {
-  //     ...jest.configs["flat/recommended"].rules,
-  //     "jest/prefer-expect-assertions": "off",
-  //   },
-  // },
+  {
+    files: ["src/tests/**/*.{js,ts}"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off",

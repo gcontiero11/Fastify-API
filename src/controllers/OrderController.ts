1085a421a311cf8ed8640fe1860094d30a63951a
gcontiero11 <gustcontiero@gmail.com>
2025-08-10 21:43:33 -0300

    refactor: streamline response handling in createOrder and createQuote methods using mappers

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -3,6 +3,8 @@ import type { CreateOrderReqModel } from "../core/schemas/order.schema";
 import { createOrderReqSchema } from "../core/schemas/order.schema";
 import OrderService from "../core/services/OrderService";
 import { ResponseException } from "../exception/ResponseException";
+import QuoteMapper from "../core/mappers/QuoteMapper";
+import OrderMapper from "../core/mappers/OrderMapper";
 
 class OrderController {
   async createOrder(req: FastifyRequest, res: FastifyReply) {
@@ -27,31 +29,33 @@ class OrderController {
       return res.status(result.getStatusCode()).send(result);
     }
 
-    const responseBody = {
-      items: result.getItems().map((item) => ({
-        productId: item.getProductId(),
-        unitPrice: item.getUnitPrice().toDecimal(),
-        quantity: item.getQuantity(),
-        subtotal: item.getSubtotal().toDecimal(),
-        category: item.getCategory(),
-        itemDiscounts: item.getItemDiscountsResponseModel(),
-      })),
-      discounts: result.getOrderDiscountsResponseModel(),
-      total: result.getTotal().toDecimal(),
-      subtotal: result.getSubtotal().toDecimal(),
-    };
+    const responseBody = OrderMapper.toResModel(result);
     return res.send(responseBody);
   }
 
   async createQuote(req: FastifyRequest, res: FastifyReply) {
+    console.log(req.body);
     const products = req.body as CreateOrderReqModel;
+
+    const parseIn = createOrderReqSchema.safeParse(products);
+
+    if (!parseIn.success) {
+      return res.status(422).send({
+        message: "Invalid products",
+        errors: parseIn.error.issues.map((issue) => ({
+          path: issue.path.join("."),
+          message: issue.message,
+        })),
+      });
+    }
     const result = await OrderService.createQuote(products);
 
     if (result instanceof ResponseException) {
       return res.status(result.getStatusCode()).send(result);
     }
 
-    return res.send(result);
+    const responseBody = QuoteMapper.toResModel(result);
+    return res.send(responseBody);
   }
 }
 
